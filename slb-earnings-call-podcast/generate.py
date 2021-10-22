3884db37a67dd666504cfd8d8660dfc445790ebd
Jan Dolejsi <JDolejsi@slb.com>
2021-10-22 18:51:32 +0200

    pubDate

--- slb-earnings-call-podcast/generate.py
+++ slb-earnings-call-podcast/generate.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import pytz
 from feedgen.feed import FeedGenerator
 fg = FeedGenerator()
 fg.load_extension('podcast')
@@ -9,6 +10,7 @@ fg.link(link={'href': "http://slb.com", 'rel': 'self'})
 fg.description("Schlumberger (SLB: NYSE) is a technology company that partners with customers to access energy. Our people, representing over 160 nationalities, are providing leading digital solutions and deploying innovative technologies to enable performance and sustainability for the global energy industry. With expertise in more than 120 countries, we collaborate to create technology that unlocks access to energy for the benefit of all.")
 fg.link(href='https://jan-dolejsi.github.io/slb-earnings-call-podcast/schlumberger-earnings-conference-calls-podcast.xml', rel='self' )
 fg.language('en')
+fg.pubDate(datetime.today().replace(tzinfo=pytz.utc))
 
 # https://investorcenter.slb.com/events/event-details/q4-2020-schlumberger-earnings-conference-call
 # https://earningscast.com/SLB/20201016
